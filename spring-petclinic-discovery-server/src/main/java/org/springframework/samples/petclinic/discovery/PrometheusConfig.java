0e810482040009062c6cd4a1011d95ff61d9554b
ars <arseniit@gmail.com>
2025-05-13 17:43:36 +0200

    Add Prometheus configuration and update application.yml for metrics exposure

---
+++ spring-petclinic-discovery-server/src/main/java/org/springframework/samples/petclinic/discovery/PrometheusConfig.java
@@ -0,0 +1,31 @@
+package org.springframework.samples.petclinic.discovery;
+
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.prometheus.client.CollectorRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration pour l'intégration avec Prometheus
+ * Cette classe permet d'exposer les métriques du Discovery Server via l'endpoint /actuator/prometheus
+ */
+@Configuration
+public class PrometheusConfig {
+
+    @Bean
+    public PrometheusMeterRegistry prometheusMeterRegistry(PrometheusConfig config, CollectorRegistry collectorRegistry, Clock clock) {
+        return new PrometheusMeterRegistry(config, collectorRegistry, clock);
+    }
+    
+    @Bean
+    public CollectorRegistry collectorRegistry() {
+        return new CollectorRegistry(true);
+    }
+    
+    @Bean
+    public Clock micrometerClock() {
+        return Clock.SYSTEM;
+    }
+}
